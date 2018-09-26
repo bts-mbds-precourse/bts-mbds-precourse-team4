ad39ec28b36f1b7d9e1320a5c7459ef32f8d0095
lctammy <43069373+lctammy@users.noreply.github.com>
2018-09-26 13:22:32 +0200

    Update wine.py

--- wine.py
+++ wine.py
@@ -3,34 +3,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-pd.set_option('display.max_rows', 5000)
-pd.set_option('display.max_columns', 50)
-pd.set_option('display.width', 1000)
 df=pd.read_csv('/Users/cenliang/Downloads/winequality-data.csv')
-df=df.drop(['id'],axis=1)
-df=df.drop(269,axis=0)
+df=df.drop(['id'],axis=1) #use default index
+df=df.drop(269,axis=0)   #outlier for residual sugar
+quality=df['quality']    #data parsing
 
 
-fixedacid=df['fixed.acidity']
-quality=df['quality']
-volatileacid=df['volatile.acidity']
-citricacid=df['citric.acid']
-resisugar=df['residual.sugar']
-chlorides=df['chlorides']
-freeso=df['free.sulfur.dioxide']
-totalso=df['total.sulfur.dioxide']
-density=df['density']
-ph=df['pH']
-sulphates=df['sulphates']
-alcohol=df['alcohol']
+#histgram
+
+def hist(feature, bin=None):
+    hist = df[feature].hist(bins=bin, grid=False, edgecolor='white', alpha=0.8, facecolor='#fc8869', figsize=(15, 20))
+    plt.ylabel(feature, fontsize=20)
+    plt.xlabel('Quantity', fontsize=20)
+    plt.show()
+    
+pd.DataFrame(list(df)).applymap(hist)
 
 
-
-#scatter functions
+#scatter plot
 
 scatterlist=[i for i in list(df) if i!='quality']
-print(list)
-
 def scatter(column):
     plt.xlabel('quality')
     plt.ylabel(column)
@@ -38,18 +30,33 @@ def scatter(column):
     plt.scatter(quality, df[column], alpha=0.4,marker='o',facecolors='none', edgecolors='r')
     plt.suptitle(column)
     plt.show()
-
+    
 pd.DataFrame(scatterlist).applymap(scatter)
+
+
+#boxplots for all data
+
+box1=df[['citric.acid', 'volatile.acidity', 'chlorides','density','sulphates']]
+box2=df[['fixed.acidity','residual.sugar','pH','alcohol']]
+box3=df[['total.sulfur.dioxide','free.sulfur.dioxide']]
+
+sns.set(style="whitegrid")
+box1 = sns.boxplot(data=box1,palette="Set1")
+plt.show()
+box2 = sns.boxplot(data=box2,palette="Set2")
+plt.show()
+box3 = sns.boxplot(data=box3,palette="Set3")
+plt.show()
+
+
 
 #heatmaps
 
 def heatmap(set,title=None):
     fig, ax = plt.subplots(figsize=(10, 10))
-    corr = set.corr()
-    sns.heatmap(corr, annot=True, annot_kws={"size": 10}, cmap="Reds",ax=ax)
+    sns.heatmap(set.corr(), annot=True, annot_kws={"size": 10}, cmap="YlGnBu",ax=ax)
     plt.title(title)
     plt.show()
-
 
 heatmap(df,title="All Wine")
 
@@ -61,43 +68,3 @@ heatmap(averagewine,title="Average Wine")
 
 lowqualitywine=df[df["quality"]<5]
 heatmap(lowqualitywine,title="Lowquality Wine")
-
-
-
-
-#histgram
-
-def hist(feature, bin=None):
-    hist = df[feature].hist(bins=bin, grid=False, edgecolor='white', alpha=0.8, facecolor='#fc8869', figsize=(30, 40))
-    plt.xlabel(feature, fontsize=20)
-    plt.ylabel('number of samples', fontsize=20)
-    plt.show()
-
-
-
-pd.DataFrame(list(df)).applymap(hist)
-
-
-#for data in df:
- #   print(hist(data))
-    
-#boxplots for all data
-
-box1=df[['citric.acid', 'volatile.acidity', 'chlorides','density','sulphates']]
-box2=df[['fixed.acidity','residual.sugar','pH','alcohol']]
-box3=df[['total.sulfur.dioxide','free.sulfur.dioxide']]
-
-sns.set(style="whitegrid")
-box1 = sns.boxplot(data=box1,palette="Set1")
-plt.show()
-
-box2 = sns.boxplot(data=box2,palette="Set2")
-plt.show()
-
-box3 = sns.boxplot(data=box3,palette="Set3")
-plt.show()
-
-
-
-
-
